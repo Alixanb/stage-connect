45f40e55055401fae8438fe48c23923da4fd33de
Nicolas ARENA <nicolasarena.nico@gmail.com>
2024-11-20 10:33:55 +0100

    routing et init

---
+++ src/Experience.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Experience: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Bienvenue dans l'expérience</h1>
+      <p>Profitez pleinement de cette expérience unique.</p>
+    </div>
+  );
+};
+
+export default Experience;
