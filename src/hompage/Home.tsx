8cdf6e6ca31215c5a146ec6cb139419220c734d8
Alixan BALU <alixanb@orange.fr>
2025-02-26 15:18:38 +0100

    header about-us

--- src/hompage/Home.tsx
+++ src/hompage/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { AboutUs } from "../components/AboutUs";
 
 const Home: React.FC = () => {
   return (
     <div className="p-16">
+      <AboutUs />
       <h1>Bienvenue sur la page d'accueil</h1>
       <p>Rejoignez une expérience immersive dès maintenant !</p>
     </div>
