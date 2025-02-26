49affda85e3458ac611534bf5d50520dee0fc682
NGUYEN <minhstrasbourg@gmail.com>
2025-02-26 10:58:27 +0100

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Experience from "./experience/Experience";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Experience from './experience/Experience'
+import Footer from './Footer'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="experience" element={<Experience />} />
-        </Route>
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="experience" element={<Experience />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
-  );
+  )
 }
