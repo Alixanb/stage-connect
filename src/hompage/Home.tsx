315df1a4263337a26f5a7965447ee925041a33d4
NGUYEN <minhstrasbourg@gmail.com>
2025-02-26 15:26:23 +0100

    Experience added

--- src/hompage/Home.tsx
+++ src/hompage/Home.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React from 'react'
+import Experience from './Experience'
 
 const Home: React.FC = () => {
   return (
     <div className="p-16">
       <h1>Bienvenue sur la page d'accueil</h1>
       <p>Rejoignez une expérience immersive dès maintenant !</p>
+      <Experience />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
