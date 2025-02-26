c31e9dbfce3db50761d570ef7abacf7a4423d222
arena <nicolasarena.nico@gmail.com>
2025-02-26 14:43:29 +0100

    section theperformer

--- src/hompage/Home.tsx
+++ src/hompage/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ThePerformer from "./ThePerformer";
 
 const Home: React.FC = () => {
   return (
     <div className="p-16">
       <h1>Bienvenue sur la page d'accueil</h1>
       <p>Rejoignez une expérience immersive dès maintenant !</p>
+      <ThePerformer />
     </div>
   );
 };
