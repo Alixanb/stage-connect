d348166df457639c09d52bc55010b550594d101c
Alixan BALU <alixanb@orange.fr>
2025-02-26 15:09:06 +0100

    homepage header

--- src/hompage/Home.tsx
+++ src/hompage/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Header } from "../components/Header";
 
 const Home: React.FC = () => {
   return (
     <div className="p-16">
+      <Header />
       <h1>Bienvenue sur la page d'accueil</h1>
       <p>Rejoignez une expérience immersive dès maintenant !</p>
     </div>
