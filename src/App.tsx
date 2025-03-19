d4ed003c6353e14c2d5a7f702850355afbd79824
Guillaume G <guillaume.gerard2112@outlook.fr>
2025-03-19 16:35:26 +0100

    Visuel OK - Envoie mail pas fonctionnel

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Experience from "./experience/Experience";
 import Home from "./hompage/Home";
 import "./index.css";
+import Contact from "./contact/Contact";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/experience" element={<Experience />} />
       </Routes>
     </BrowserRouter>
