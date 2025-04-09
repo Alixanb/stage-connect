b7b26423c4ff33bcc323b5fed8289c741af6fbf6
Alixan BALU <alixanb@orange.fr>
2025-04-09 11:15:51 +0200

    merge modif

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-=======
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
   assetsInclude: ["**/*.glb"],
->>>>>>> rfc
 });
