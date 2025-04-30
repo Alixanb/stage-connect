9305c17e230e4ca5a86b10c5aeed63903ec13823
Guillaume G <guillaume.gerard2112@outlook.fr>
2025-04-30 09:41:30 +0200

    start police

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,22 @@ export default {
         primary: "#86182F",
       },
       fontFamily: {
+        nickel: ["NickelGothic", "sans-serif"],
         inter: ["Inter", "sans-serif"],
       },
       fontSize: {
-        'xs': '0.75rem',     // 12px
-        'sm': '0.875rem',    // 14px
-        'base': '1rem',      // 16px
-        'lg': '1.125rem',    // 18px
-        'xl': '1.25rem',     // 20px
-        '2xl': '1.5rem',     // 24px
-        '3xl': '1.875rem',   // 30px
-        '4xl': '2.25rem',    // 36px
-        '5xl': '3rem',       // 48px
-        '6xl': '3.75rem',    // 60px
-        '7xl': '4.5rem',     // 72px
-        '8xl': '6.25rem',    // 100px
+        xs: "0.75rem", // 12px
+        sm: "0.875rem", // 14px
+        base: "1rem", // 16px
+        lg: "1.125rem", // 18px
+        xl: "1.25rem", // 20px
+        "2xl": "1.5rem", // 24px
+        "3xl": "1.875rem", // 30px
+        "4xl": "2.25rem", // 36px
+        "5xl": "3rem", // 48px
+        "6xl": "3.75rem", // 60px
+        "7xl": "4.5rem", // 72px
+        "8xl": "6.25rem", // 100px
       },
     },
   },
