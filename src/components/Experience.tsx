cdd00c8b6cf9a057dda0609f7ff7c74335b4a81f
Alixan BALU <alixanb@orange.fr>
2025-04-30 16:19:01 +0200

    slay

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -56,7 +56,7 @@ export const Experience = () => {
         <Map
           scale={mapConfig.scale}
           position={mapConfig.position}
-          model={`models/${map}.glTF`}
+          model={`models/${map}.glb`}
         />
         <CharacterController />
         <InteractiveElements />
